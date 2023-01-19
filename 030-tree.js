87ed2c163f0d90d643a464129f5ec47b079b1109
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-19 19:48:02 +0600

    tree completed

--- 030-tree.js
+++ 030-tree.js
@@ -102,22 +102,88 @@ class BinarySearchTree {
       console.log(root.value);
     }
   }
+
+  // Breadth First Search
+  levelOrder() {
+    const queue = [];
+    queue.push(this.root);
+
+    while (queue.length) {
+      let cur = queue.shift();
+      console.log(cur.value);
+      if (cur.left) {
+        queue.push(cur.left);
+      }
+      if (cur.right) {
+        queue.push(cur.right);
+      }
+    }
+  }
+
+  min(root) {
+    if (!root.left) {
+      return root.value;
+    } else {
+      return this.min(root.left);
+    }
+  }
+
+  max(root) {
+    if (!root.right) {
+      return root.value;
+    } else {
+      return this.max(root.right);
+    }
+  }
+
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+
+  deleteNode(root, value) {
+    if (root === null) {
+      return root;
+    }
+    if (value < root.value) {
+      root.left = this.deleteNode(root.left, value);
+    } else if (value > root.value) {
+      root.right = this.deleteNode(root.right, value);
+    } else {
+      if (!root.left && !root.right) {
+        return null;
+      }
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+      root.value = this.min(root.right);
+      root.right = this.deleteNode(root.right, root.value);
+    }
+    return root;
+  }
 }
 
 const bst = new BinarySearchTree();
-console.log(bst.isEmpty());
+// console.log(bst.isEmpty());
 
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
 bst.insert(3);
-bst.insert(7);
+// bst.insert(7);
 
-console.log(bst.search(bst.root, 10));
-console.log(bst.search(bst.root, 5));
-console.log(bst.search(bst.root, 15));
-console.log(bst.search(bst.root, 20));
+// console.log(bst.search(bst.root, 10));
+// console.log(bst.search(bst.root, 5));
+// console.log(bst.search(bst.root, 15));
+// console.log(bst.search(bst.root, 20));
 
 // bst.preOrder(bst.root);
 // bst.inOrder(bst.root);
-bst.postOrder(bst.root);
+// bst.postOrder(bst.root);
+// bst.levelOrder();
+// console.log(bst.min(bst.root));
+// console.log(bst.max(bst.root));
+
+bst.delete(10);
+bst.levelOrder();
